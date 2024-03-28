c7305f4977bd9279815b66150d5ecaf747627fc8
Elasia H <elasia.hamilton@outlook.com>
2024-03-28 18:46:30 -0500

    basic homepage layout, added bootstrap

--- Client/src/App.vue
+++ Client/src/App.vue
@@ -1,14 +1,25 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+import NavBar from './components/NavBar.vue'
 import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
+  <header>
+    <NavBar />
+  </header>
   <RouterView />
 </template>
 
 <style scoped>
 header {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+}
+
+/* header {
   line-height: 1.5;
   max-height: 100vh;
 }
@@ -68,5 +79,5 @@ nav a:first-of-type {
     padding: 1rem 0;
     margin-top: 1rem;
   }
-}
+} */
 </style>
---
+++ Client/src/components/HeroSection.vue
@@ -0,0 +1,47 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="heroSection">
+    <div class="heroContainer">
+      <div class="heroText">
+        <h1 class="text-white">Text Here</h1>
+      </div>
+      <div class="heroSubtitle">
+        <p>Lorem Ipsum et cetera</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.heroSection {
+  background-color: #297373;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  position: relative;
+  min-height: 500px;
+  width: 100%;
+}
+
+.heroContainer {
+  margin: auto;
+  max-width: 1280px;
+}
+
+.heroSubtitle {
+  color: limegreen;
+}
+
+.text-white {
+  color: white;
+}
+</style>
