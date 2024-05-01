9078f1394543cbc02e17e6273e3ef41ec4322ba2
ristianca <chmyeducationemail@gmail.com>
2024-05-01 17:21:03 -0500

    Commenting

--- Server/SQL-Setup/Users-Table.sql
+++ Server/SQL-Setup/Users-Table.sql
@@ -1,9 +1,13 @@
 Create Table USERS (
+    ##Primary ID
     ID int,
+    ##Permissions based ID associated with user
     UID varchar(255),
     First_Name varchar(255),
     Last_name varchar(255),
     Email varchar(255),
+    ##For redirect purposes 
     Registration_Done binary default 0,
+    ## To check if this an admin user
     Admin binary default 0,
 )
