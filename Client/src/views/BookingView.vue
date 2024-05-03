3947f9b9fa1c204d37f3e106a0127b2c44a29d78
Dillon Greek <techhead404@gmail.com>
2024-05-03 11:42:52 -0500

    Iframe

--- Client/src/views/BookingView.vue
+++ Client/src/views/BookingView.vue
@@ -3,8 +3,7 @@
     <div class="h-100vh w-100vw mx-auto">
       <h1>Upcoming events:</h1>
       <pre id="content"></pre>
-      <p>Click the button below to list upcoming events:</p>
-      <button @click="listUpcomingEvents">Click here</button>
+      <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=America%2FChicago&bgcolor=%23039BE5&showPrint=0&showCalendars=0&showTz=0&showTabs=0&showTitle=0&src=Ym9va2luZ0BoZWFsaW5ncGVhY2VtYXNzYWdlLmNvbQ&src=YWRkcmVzc2Jvb2sjY29udGFjdHNAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&src=ZW4udXNhI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&color=%23039BE5&color=%2333B679&color=%230B8043" style="border:solid 1px #777" width="800" height="600" frameborder="0" scrolling="no"></iframe>
     </div>
   </body>
 </template>
